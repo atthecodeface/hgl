51ce0a48ecaa4f81d52c07cd7e6d267ead15c4eb
Gavin Stark <atthecodeface@gmail.com>
2025-03-04 11:51:56 +0000

    Moved SimEdgeMask to its own file

---
+++ rust/sim/src/simulation/edge_mask.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, Default, Clone, Copy)]
+pub struct SimEdgeMask(u64);
+impl SimEdgeMask {
+    pub fn none() -> Self {
+        Self(0)
+    }
+    pub fn is_none(&self) -> bool {
+        self.0 == 0
+    }
+    pub const fn is_posedge(&self, input: usize) -> bool {
+        ((self.0 as usize) >> (input * 2)) & 1 != 0
+    }
+    pub const fn is_negedge(&self, input: usize) -> bool {
+        ((self.0 as usize) >> (input * 2)) & 2 != 0
+    }
+    pub fn add_posedge(mut self, input: usize) -> Self {
+        self.0 |= (1 << (2 * input)) as u64;
+        self
+    }
+    pub fn add_negedge(mut self, input: usize) -> Self {
+        self.0 |= (2 << (2 * input)) as u64;
+        self
+    }
+    pub fn set_posedge(&mut self, input: usize) {
+        self.0 |= (1 << (2 * input)) as u64;
+    }
+    pub fn set_negedge(&mut self, input: usize) {
+        self.0 |= (2 << (2 * input)) as u64;
+    }
+}
--- rust/sim/src/simulation/mod.rs
+++ rust/sim/src/simulation/mod.rs
@@ -1,5 +1,6 @@
 //a Modules
 mod clock;
+mod edge_mask;
 mod instance;
 mod instance_ref;
 mod names;
@@ -8,6 +9,7 @@ mod simulation;
 
 //a Exports
 pub use clock::{Clock, ClockArray, ClockIndex};
+pub use edge_mask::SimEdgeMask;
 pub use instance::{Instance, InstanceHandle};
 pub use instance_ref::{RefInstance, RefMutInstance};
 pub use names::{Name, NameFmt, Names, NamespaceStack, NsNameFmt, SimNsName};
@@ -18,31 +20,4 @@ pub use simulation::{Simulation, SimulationBody, SimulationControl};
 //tp SimReset
 pub enum SimReset {
     Restart,
-}
-#[derive(Debug, Default, Clone, Copy)]
-pub struct SimEdgeMask(u64);
-impl SimEdgeMask {
-    pub fn is_none(&self) -> bool {
-        self.0 == 0
-    }
-    pub const fn is_posedge(&self, input: usize) -> bool {
-        ((self.0 as usize) >> (input * 2)) & 1 != 0
-    }
-    pub const fn is_negedge(&self, input: usize) -> bool {
-        ((self.0 as usize) >> (input * 2)) & 2 != 0
-    }
-    pub fn add_posedge(mut self, input: usize) -> Self {
-        self.0 |= (1 << (2 * input)) as u64;
-        self
-    }
-    pub fn add_negedge(mut self, input: usize) -> Self {
-        self.0 |= (2 << (2 * input)) as u64;
-        self
-    }
-    pub fn set_posedge(&mut self, input: usize) {
-        self.0 |= (1 << (2 * input)) as u64;
-    }
-    pub fn set_negedge(&mut self, input: usize) {
-        self.0 |= (2 << (2 * input)) as u64;
-    }
 }
