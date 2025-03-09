d8513f1b45c07e31106408cbaa15014e7893bddb
Gavin Stark <atthecodeface@gmail.com>
2025-03-09 15:16:02 +0000

    Added impl of Idx for usize

--- rust/indexed_vec/src/index.rs
+++ rust/indexed_vec/src/index.rs
@@ -1,10 +1,24 @@
 //a Idx
 //tt Idx
+/// The trait for an index
+///
+/// This *explicitly* does not support `From<usize>`; to create an
+/// index type from a usize, use the `from_usize()` method. Supporting
+/// From, and hence Into, would weaken the protection provided by the
+/// index type, so it *must* be opt-in by the type.
 pub trait Idx:
     Copy + std::fmt::Debug + PartialEq + Eq + PartialOrd + Ord + std::hash::Hash + 'static
 {
     fn from_usize(idx: usize) -> Self;
     fn index(self) -> usize;
+}
+impl Idx for usize {
+    fn from_usize(n: usize) -> usize {
+        n
+    }
+    fn index(self) -> usize {
+        self
+    }
 }
 
 //a Macro make_index
